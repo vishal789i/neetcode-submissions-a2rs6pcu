ca8eef161658f36e2c87c7fced1e13b54c7306ff
vishal789i <45600431+vishal789i@users.noreply.github.com>
2026-04-19 00:47:43 +0530

    Add: reconstruct-flight-path - submission-10

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-10.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        sort(tickets.begin(), tickets.end());
+        map<string, vector<string>> adj;
+        for (auto& t : tickets) {
+            adj[t[0]].push_back(t[1]);
+        }
+ 
+        vector<string> res{"JFK"};
+        function<bool(string)> dfs = [&] (string cur) {
+            if (res.size() == tickets.size() + 1) {
+                return true;
+            }
+
+            if (not adj.contains(cur)) {
+                return false;
+            }
+
+            auto temp = adj[cur];
+            for (int i = 0; i < temp.size(); ++i) {
+                string nextV = temp[i];
+                adj[cur].erase(adj[cur].begin() + i);
+                res.push_back(nextV);
+                if (dfs(nextV)) {
+                    return true;
+                }
+
+                res.pop_back();
+                adj[cur].insert(adj[cur].begin() + i, nextV);
+            }
+
+            return false;
+        };
+
+        dfs("JFK");
+        return res;
+    }
+};
