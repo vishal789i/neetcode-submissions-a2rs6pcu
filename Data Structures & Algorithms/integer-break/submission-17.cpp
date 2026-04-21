12a693f9823e87a654ac1d926db08431879ab222
vishal789i <45600431+vishal789i@users.noreply.github.com>
2026-04-21 21:23:16 +0530

    Add: integer-break - submission-17

---
+++ Data Structures & Algorithms/integer-break/submission-17.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector dp(n + 1, 0);
+        dp[1] = 1;
+        for (int j = 2; j <= n; ++j) {
+            int res = 1 * (j - 1);
+            for (int i = 2; i <= j / 2; ++i) {
+                int prod = i * max(j - i, dp[j - i]);
+                res = max(res, prod);
+            }
+
+            dp[j] = res;
+        }
+
+        return dp[n];
+    }
+};
