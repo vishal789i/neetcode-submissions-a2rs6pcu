89a5f9dbe4ea9ed309a56f68946b8006fb14a9fa
vishal789i <45600431+vishal789i@users.noreply.github.com>
2026-04-27 13:35:11 +0530

    Add: meeting-rooms-iii - submission-1

---
+++ Data Structures & Algorithms/meeting-rooms-iii/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        vector<int> roomUsedCount(n);
+        vector<long long> lastAvailableAt(n);
+        for (auto& meeting : meetings) {
+            auto start = meeting[0], end = meeting[1], duration = end - start;
+            bool found = false;
+            long long timeOfEarliestEndingRoom = LLONG_MAX;
+            int earliestEndingRoom = -1;
+            for (auto room = 0; room < n; ++room) {
+                if (lastAvailableAt[room] <= start) {
+                    lastAvailableAt[room] = end;
+                    roomUsedCount[room]++;
+                    found = true;
+                    break;
+                }
+
+                if (lastAvailableAt[room] < timeOfEarliestEndingRoom) {
+                    timeOfEarliestEndingRoom = lastAvailableAt[room];
+                    earliestEndingRoom = room;
+                }
+            }
+
+            if (not found) {
+                lastAvailableAt[earliestEndingRoom] += duration;
+                roomUsedCount[earliestEndingRoom]++;
+            }
+        }
+
+        return distance(roomUsedCount.begin(), max_element(roomUsedCount.begin(), roomUsedCount.end()));
+    }
+};
