ac919ed4e305fa8284b57e76467b82013b4085e8
vishal789i <45600431+vishal789i@users.noreply.github.com>
2026-05-14 00:39:24 +0530

    Add: find-words-that-can-be-formed-by-characters - submission-0

---
+++ Data Structures & Algorithms/find-words-that-can-be-formed-by-characters/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        vector<int> charCnts(26);
+        for (auto c : chars) {
+            charCnts[c - 'a']++;
+        }
+
+        int res = 0;
+        for (auto& w : words) {
+            vector<int> wCnts(26);
+            for (auto c : w) {
+                wCnts[c - 'a']++;
+            }
+            /*
+                a 2
+                c 1
+                h 1
+                t 1
+
+                a 1
+                c 1
+                t 1
+            */
+
+            bool match = true;
+            for (int i = 0; i < 26; ++i) {
+                if (wCnts[i] != 0 and wCnts[i] > charCnts[i]) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                res += w.size();
+            }
+        }
+
+        return res;
+    }
+};
