4f5275e0bff7b734f90a21420cc250812d1ee53b
vishal789i <45600431+vishal789i@users.noreply.github.com>
2026-06-08 15:13:59 +0530

    Add: restore-ip-addresses - submission-4

---
+++ Data Structures & Algorithms/restore-ip-addresses/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        function<void(int, string, int)> bt = [&] (int i, string op, int dots) {
+            if (i == s.size() and dots == 4) {
+                op.pop_back();
+                res.push_back(op);
+                return;
+            }
+
+            if (dots > 3) {
+                return;
+            }
+
+            string tmp;
+            for (int j = i; j < min((int)s.size(), i + 3); ++j) {
+                if (j > i and s[i] == '0') {
+                    continue;
+                }
+                
+                tmp += s[j];
+                if (stoi(tmp) <= 255) {
+                    bt(j + 1, op + tmp + '.', dots + 1);
+                }
+            }
+        };
+
+        bt(0, "", 0);
+        return res;
+    }
+};
