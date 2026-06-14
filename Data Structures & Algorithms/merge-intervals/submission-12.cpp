8a1aca82cd64f3e86bc9f0e87e7c9d8da82db03c
vishal789i <45600431+vishal789i@users.noreply.github.com>
2026-06-15 02:26:18 +0530

    Add: merge-intervals - submission-12

---
+++ Data Structures & Algorithms/merge-intervals/submission-12.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        map<int, int> events;
+        for (auto& i : intervals) {
+            events[i[0]]++;
+            events[i[1]]--;
+        }
+
+        vector<vector<int>> res;
+        int sum = 0;
+        int start = -1;
+        for (auto [e, cnt] : events) {
+            if (start == -1) {
+                start = e;
+            }
+
+            sum += cnt;
+            if (sum == 0) {
+                res.push_back({start, e});
+                start = -1;
+            }
+        }
+
+        return res;
+    }
+};
